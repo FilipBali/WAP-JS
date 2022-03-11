9bc9548bda442be6a77c82e96b5d2c450b0f0849
Filip Bali <14928466+FilipBali@users.noreply.github.com>
2022-03-11 20:31:53 +0100

    Update tree.mjs

--- tree.mjs
+++ tree.mjs
@@ -1,39 +1,65 @@
 function Tree(expr){
 
     this.fexpr = expr
-    this.root = new EmptyTree(expr)
+    this.data = new EmptyTree(expr)
     // this.left = new EmptyTree(expr)
     // this.right = new EmptyTree(expr)
 
     Tree.prototype.insertValue = function(value){
         // console.log(value);
 
-        let t = this.root
-        this.root = t.insertValue(value)
+        let t = this.data
+        this.data = t.insertValue(value)
 
     }
 
-    function preorder(){
+    Tree.prototype.preorder = function(){
+        // this.data.preorder()
 
+        const gen = this.data.preorder()
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
     }
-    function inorder(){
-
+    Tree.prototype.inorder = function(){
+        const gen = this.data.inorder()
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
     }
-    function postorder(){
-
+    Tree.prototype.postorder = function(){
+        const gen = this.data.postorder()
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
     }
 }
 
 function ValueTree(expr, value){
     this.fexpr = expr
-    this.root = value
+    this.data = value
     this.left = new EmptyTree(expr)
     this.right = new EmptyTree(expr)
 
     ValueTree.prototype.insertValue = function(value){
         let tmp;
         let newT;
-        if (this.fexpr(value, this.root)) {
+        if (this.fexpr(value, this.data)) {
             newT = this.left
             tmp = newT.insertValue(value)
             this.left = tmp
@@ -44,12 +70,35 @@ function ValueTree(expr, value){
         }
         return this
     }
+
+
+    // ValueTree.prototype.preorder = function(){
+    ValueTree.prototype.preorder = function*(){
+        // console.log(this.data)
+        // this.left.preorder()
+        // this.right.preorder()
+
+        yield this.data
+        yield *this.left.preorder()
+        yield *this.right.preorder()
+    }
+
+    ValueTree.prototype.inorder = function*(){
+        yield *this.left.preorder()
+        yield this.data
+        yield *this.right.preorder()
+    }
+    ValueTree.prototype.postorder = function*(){
+        yield *this.left.preorder()
+        yield *this.right.preorder()
+        yield this.data
+    }
 }
 
 
 function EmptyTree(expr){
     this.fexpr = expr
-    this.root = null
+    this.data = null
     this.left = null
     this.right = null
 
@@ -57,11 +106,22 @@ function EmptyTree(expr){
         let nt = new ValueTree(this.fexpr, value);
         return nt
     }
+
+    // EmptyTree.prototype.preorder = function(){
+    EmptyTree.prototype.preorder = function*(){}
+    EmptyTree.prototype.inorder = function*(){}
+    EmptyTree.prototype.postorder = function*(){}
 }
 
 let t = new Tree((a,b) => a < b);
 console.log( t.fexpr(2,1) )
 t.insertValue(3)
-t.insertValue(2)
+t.insertValue(9)
+t.insertValue(10)
+t.insertValue(15)
+t.insertValue(1)
 t.insertValue(4)
 t.insertValue(6)
+t.preorder()
+t.inorder()
+t.postorder()
